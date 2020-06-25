4f6c868a33f37f7eb5780dfeb9f4674eeea667c4
Dennis Collaris <d.collaris@me.com>
2020-06-25 20:22:44 +0200

    fix: replace userInterfaceIdiom with size classes

--- ImagePicker/Sources/ImagePicker.swift
+++ ImagePicker/Sources/ImagePicker.swift
@@ -126,13 +126,11 @@ public class ImagePicker: UICollectionView {
     landscapeConstraint?.priority = isLandscape ? .defaultHigh : .defaultLow
     landscapeConstraint?.isActive = true
 
-    NotificationCenter.default.addObserver(self, selector: #selector(ImagePicker.rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
-
     provider?.fetchAssets(for: mediaType)
     reloadData()
   }
 
-  @objc func rotated() {
+  public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
     updatePreviewHeight()
 
     landscapeConstraint?.priority = isLandscape ? .defaultHigh : .defaultLow
@@ -179,14 +177,16 @@ public class ImagePicker: UICollectionView {
   }
 
   func calcHeight(for alertController: UIAlertController?) -> CGFloat {
-    guard let alertController = alertController else { return 0 }
+    guard let window = window, let alertController = alertController else { return 0 }
 
-    if UIDevice.current.userInterfaceIdiom == .phone && isLandscape {
+    if traitCollection.verticalSizeClass == .compact {
       return 65
     }
 
     var height = alertController.actions.filter { $0.style != .cancel }.map { _ in CGFloat(58) }.reduce(0, +)
-    if alertController.actions.contains(where: { $0.style == .cancel }) && UIDevice.current.userInterfaceIdiom == .phone  {
+
+
+    if alertController.actions.contains(where: { $0.style == .cancel }) && window.traitCollection.horizontalSizeClass == .compact {
       height += 65
     }
     return height
